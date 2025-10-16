68935063eeee6df8f50b3c64d64ea950ef4b46fe
yehoon00 <rainer8191@gmail.com>
2025-10-16 20:11:55 +0900

    feat : 입력 및 입력값에 공백 제거 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+
+// 최대한 객체 지향적으로 작성
+// todo
+// 입력 -> 판별 -> 덧셈 -> 결과 순으로 리턴
+
 class App {
-  async run() {}
+  async run() {
+    this.getInput();
+  }
+
+  async getInput() {
+    const input_Value = await Console.readLineAsync("숫자를 입력하세요 : ");
+
+    const trim_Input = input_Value.replaceAll(" ", "");
+
+    return Console.print(trim_Input);
+  }
 }
 
 export default App;
