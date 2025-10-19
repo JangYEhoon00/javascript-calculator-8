b350857f197ea90a2252851daa10400aeb644ec7
yehoon00 <rainer8191@gmail.com>
2025-10-19 22:09:05 +0900

    refactor : 코드규모에 따른 클래스 구분

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
 
-// 최대한 객체 지향적으로 작성
-// todo
-// 입력 -> 판별 -> 덧셈 -> 결과 순으로 리턴
-
 class App {
   async run() {
-    this.parser = new Parser();
-    this.calculator = new Calculator();
+    this.inputCalculator = new IputCalculator();
 
-    const input = await this.getInput();
-    const numbers = this.parser.parse(input);
-    const result = this.calculator.add(numbers);
+    const input = await this.inputCalculator.getInput();
+    const numbers = this.inputCalculator.parse(input);
+    const result = this.inputCalculator.add(numbers);
 
     Console.print(`결과 : ${result}`);
   }
+}
 
+class IputCalculator {
   async getInput() {
     const input_Value = await Console.readLineAsync("숫자를 입력하세요 : ");
     const trim_Input = input_Value.replaceAll(" ", "");
 
     return trim_Input;
   }
-}
 
-class Calculator {
   add(numbers) {
     let sum = 0;
     for (let arr of numbers) {
@@ -32,12 +27,11 @@ class Calculator {
     }
     return sum;
   }
-}
 
-class Parser {
   parse(trimedInput) {
     let temp = "";
     let numArr = [];
+
     for (let i = 0; i < trimedInput.length; ++i) {
       const to_Char = trimedInput[i];
 
